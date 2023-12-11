fd766f4aaea93fae52b690a26a612fb27d9f2f2b
FAIZAN SHAIK <lordfaizu123@gmail.com>
2023-12-11 11:42:56 +0000

    strings done

---
+++ 02_strings/2_string_methods.js
@@ -0,0 +1,18 @@
+//slice
+
+let myName = "faizan"
+console.log(myName.slice(1))
+console.log(myName.slice(1,3))
+console.log(myName.slice(1,-3))
+
+
+//substring()
+console.log(myName.substring(1,2))
+console.log(myName.substring(1))
+console.log(myName.substring(-3))
+
+//substr()
+console.log(myName.substr(2,6).length)
+
+//split()
+console.log(myName.split('i'))
