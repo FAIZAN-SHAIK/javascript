9eeb0cc1f4ca66a0be2d8d0cf0c01c58cab4098c
FAIZAN SHAIK <lordfaizu123@gmail.com>
2023-12-11 11:18:21 +0000

    new

---
+++ 03_loops/2_for.js
@@ -0,0 +1,10 @@
+// for(var i = 0;i<=10;i++){
+//     console.log(i);
+// }
+
+// console.log(i)
+var sum = 0
+for(var i = 1;i<=10;i++){
+    sum = sum+i;
+}
+console.log(sum)
---
+++ 04_arrays/01_array_intro.js
@@ -0,0 +1,9 @@
+var myArr1 = [1,2,3,4]
+var myArr2 = ["faizan","jhsd","wjkqo"]
+var myArr3 = ["faizan","jhsd","wjkqo",1,2,2.3,null,undefined]
+
+console.log(myArr1)
+console.log(myArr2)
+console.log(myArr3)
+
+console.log(Array.isArray(myArr1))
